48e515a4d929e6ad87d347afc35f7a50c51dd872
kaori <22031037@zju.edu.cn>
2023-01-29 01:58:11 +0800

    Update Camera constructor

--- LearnOpenGL/src/Renderer/camera.h
+++ LearnOpenGL/src/Renderer/camera.h
@@ -11,7 +11,7 @@ enum camera_movement {
 class Perspective_Camera
 {
 public:
-	Perspective_Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 at = glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float fov = 45.0f, glm::vec3 euler = glm::vec3(0.0f, -90.0f, 0.0f));
+	Perspective_Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 euler = glm::vec3(0.0f, -90.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float fov = 45.0f);
 
 	void process_key_event(camera_movement direction, float delta_time);
 	void process_mouse_event(float x_offset, float y_offset, GLboolean constrainPitch = true);
@@ -19,6 +19,8 @@ public:
 	
 	void set_camera_speed(float speed) { m_camera_speed = speed; }
 	void set_mouse_sensitivity(float sensitivity) { m_mouse_sensitivity = sensitivity; }
+	void set_possition(glm::vec3 position) { m_position = position; }
+	void set_euler(glm::vec3 euler) { m_euler = euler; }
 
 	const glm::mat4 get_view_matrix() const { return m_view_matrix; }
 	const glm::mat4 get_view_projection_matrix() const { return m_view_projection_matrix; }
@@ -29,10 +31,10 @@ private:
 	void update_camera_space_vector();
 private:
 	glm::vec3 m_position;
+	glm::vec3 m_world_up;
 	glm::vec3 m_front;
 	glm::vec3 m_up;
 	glm::vec3 m_right;
-	glm::vec3 m_world_up;
 	glm::vec3 m_euler;
 
 	float m_fov;
